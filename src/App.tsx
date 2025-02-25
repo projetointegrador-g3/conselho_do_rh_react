644b5f17006cebb00a21e2fe6133092cdba42fbc
CHEKE01 <134391765+CHEKE01@users.noreply.github.com>
2025-02-25 16:39:44 -0300

    :sparckles: atualizando App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
           <Navbar />
           <div className="min-h-[80vh]">
           </div>
+          <Footer/>
         </BrowserRouter>
     </>
   )
