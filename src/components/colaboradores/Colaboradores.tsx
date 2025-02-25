2beb02d7b27fb053808501e10316cd72601d8473
CHEKE01 <134391765+CHEKE01@users.noreply.github.com>
2025-02-25 15:51:38 -0300

    :sparkle: modelo Navbar

--- src/components/colaboradores/Colaboradores.tsx
+++
@@ -1,4 +0,0 @@
-<div className="hjohsdpgaidugsaidg">sldojhaodsga
-    sajdohaohds
-    
-</div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,1 +1,22 @@
-.
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-yellow-100 text-gray-800'>
+            
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Conselho do RH</Link>
+
+                    <div className='flex gap-4'>
+                        Colaboradores
+                        Sobre
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
