f9fb064d93379f122b20607d74b18cd9ed212b0d
CHEKE01 <134391765+CHEKE01@users.noreply.github.com>
2025-02-25 16:12:01 -0300

    :sparcke: criação Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
-
   return (
     <>
-      
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+          </div>
+        </BrowserRouter>
     </>
   )
 }
