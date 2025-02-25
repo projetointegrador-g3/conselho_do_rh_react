ce2eca0655f049a9bb09dbe7a79738d15f3300d8
Emily <emilys@genstudents.org>
2025-02-25 16:38:58 -0300

    :card_file_box: Configurando data

---
+++ src/data/images.ts
@@ -0,0 +1,5 @@
+export const images = [
+    'https://ik.imagekit.io/n0nz1jfh6/rh-front/img2.jpg?updatedAt=1740508007041',
+    'https://ik.imagekit.io/n0nz1jfh6/rh-front/img4.jpg?updatedAt=1740512051739',
+  
+  ]
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,39 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel } from 'react-bootstrap';
+import { images } from '../../data/images';
 
-// Array com Imgs
-const images = [
-  'https://ik.imagekit.io/n0nz1jfh6/Comes%20e%20bebes/Torta%20de%20maçã.jpeg?updatedAt=1737559633455',
-  'https://ik.imagekit.io/n0nz1jfh6/Comes%20e%20bebes/Brigadeiro.jpeg?updatedAt=1737559633078',
-  'https://ik.imagekit.io/n0nz1jfh6/Comes%20e%20bebes/Pizza%20Marguerita.jpeg?updatedAt=1737559633363',
-  'https://ik.imagekit.io/n0nz1jfh6/Comes%20e%20bebes/Frango%20assado%20com%20batatas.jpeg?updatedAt=1737559632917',
-  'https://ik.imagekit.io/n0nz1jfh6/Comes%20e%20bebes/Salada%20Caesar.jpeg?updatedAt=1737559632957'
-];
-
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <>
-       <div className="bg-container">
-      <Carousel indicators={false} controls={false} interval={1000} className="carousel">
+    <div className='bg_container'>
+
+      <Carousel indicators={false} controls={false} interval={1000} className='carousel'>
         {images.map((image, index) => (
           <Carousel.Item key={index}>
             <div
-              className="d-block w-100 bg-image"
-              style={{ backgroundImage: `url(${image})` }}
-            />
+              className='d-block w-100 bg_image'
+              style={{ backgroundImage: `url(${image})` }} />
           </Carousel.Item>
         ))}
-      </Carousel>
+      </Carousel> 
 
-      <div className="content">
-        <h1>Bem-vindo à minha aplicação</h1>
-        <p>Esta é a página inicial.</p>
-      </div>
-      
-    </div>    
-    </>
-  )
-}
+          <div className='content'>
+            <h1 className='text_title_home'>
+            Inovação na atração de talentos
+            </h1>
+            <p className=''>
+            Com a CDR, você poupa o esforço do seu RH e impulsiona eficiência em todo processo, 
+            recrutando com agilidade e usando métodos estruturados para selecionar com responsabilidade.
+            </p>
+          </div>
+    </div>
+  );
+};
 
-export default Home
-
+export default Home;
