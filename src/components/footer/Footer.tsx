0572bbff1ac21d4093c4aa72e6f9671518d25e7b
Emily <emilys@genstudents.org>
2025-02-26 10:29:32 -0300

    :sparkles: Footer components

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,20 @@
-import { ArrowDown, FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 import styles from './Footer.module.css'
+import { ChevronDown } from "lucide-react";
+import { useState } from 'react';
 
 function Footer() {
 
     const data = new Date().getFullYear()
+    const [isOpen, setIsOpen] = useState(false);
+    const [language, setLanguage] = useState("Portuguese");
+
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+    const changeLanguage = (lang) => {
+        setLanguage(lang);
+        setIsOpen(false);
+    };
 
     return (
         <>
@@ -20,8 +31,28 @@ function Footer() {
                     <p className='text-lg font-extralight m-1'>Desk Dundies © {data}</p>
                     <p className='text-lg font-extralight m-0'>Conectando talentos às melhores oportunidades. 
                         Sua plataforma confiável para soluções em recrutamento e gestão de pessoas. </p>
-                    <p className='text-lg font-extralight m-1'> Brazil | R$  Portuguese ⬇</p>
-                        
+                    <p className='text-lg font-extralight m-1'> Brazil | R$</p>
+                    
+
+                    {/* Dropdown de Idioma */}
+                    <div className="relative mt-2 text-sm">
+                        <button 
+                            onClick={toggleDropdown} 
+                            className="flex items-center gap-2 border p-2 rounded cursor-pointer"
+                        >
+                            {language} <ChevronDown size={16} />
+                        </button>
+
+                        {isOpen && (
+                            <div className="absolute left-0 mt-1 w-40 bg-black shadow-md rounded-md border z-10">
+                                <ul className="p-2">
+                                    <li className="cursor-pointer p-2 hover:bg-gray-900" onClick={() => changeLanguage("Portuguese")}>🇧🇷 Português</li>
+                                    <li className="cursor-pointer p-2 hover:bg-gray-900" onClick={() => changeLanguage("English")}>🇺🇸 English</li>
+                                    <li className="cursor-pointer p-2 hover:bg-gray-900" onClick={() => changeLanguage("Español")}>🇪🇸 Español</li>
+                                </ul>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </>
